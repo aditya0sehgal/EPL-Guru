80130cf1e9041fba9a2056a665d202868d16b188
aditya0sehgal <aditya0sehgal.dev@gmail.com>
2023-07-26 19:59:02 -0400

    added year and teams dropdown

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pandas as pd
+import random, numpy, csv, io, os, json
 import time
 
 app = Flask(__name__)
 
+# Home landing page
+@app.route("/" , methods=['GET'])
+def home():
+    return render_template('home.html')
 
-@app.route('/')
-def hello():
-    return '<h1>Hello!</h1>'
-
-
-@app.route('/about/')
+@app.route('/players')
 def about():
-    return '<h3>This is a Flask web application.</h3>'
+    return render_template('player_info.html')
 
-      
 @app.route('/aboutnew/')
 def aboutnew():
     return '<h3>This is a new Flask web application.</h3>'
-
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
